c71362142d4216e9cd17a8ae98a16f7e04666042
tomorussell0315-cmd <tomo.russell0315@gmail.com>
2026-08-06 10:52:33 +0900

    Update CACHE_NAME to study-ledger-v6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'study-ledger-v3';
+const CACHE_NAME = 'study-ledger-v6';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', (event) => {
