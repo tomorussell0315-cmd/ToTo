6566f0298ea068bc16d65cfc9baecaf85d7dc74d
tomorussell0315-cmd <tomo.russell0315@gmail.com>
2026-08-06 15:45:25 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'study-ledger-v6';
+const CACHE_NAME = 'study-ledger-v7';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', (event) => {
